1103238f5fd12151f0a00aade4f313a39744d4e9
esmerqedirli <smrqdirli14@gmail.com>
2026-03-30 11:47:20 -0700

    feat: implement generic repository, unit of work with transactions and service registration

---
+++ Infrastructure/SkillBridge.Infrastructure/Persistence/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillBridge.Infrastructure.Persistence.UnitOfWork
+{
+    internal class UnitOfWork
+    {
+    }
+}
